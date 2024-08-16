79e7dbef26b643659567e46dfabee078084efa7d
SergRusetskiy1974 <sergrus1974@gmail.com>
2024-08-16 19:48:16 +0300

    init data science

--- DataScience/start_ds.py
+++ DataScience/start_ds.py
@@ -1,40 +1,51 @@
-
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+plate_cascade = cv2.CascadeClassifier("./plate_detect_models/indian_license_plate.xml")
 
-plate_cascade = cv2.CascadeClassifier('./plate_detect_models/indian_license_plate.xml')
 
-
-def detect_plate(img, text=''):  # the function detects and perfors blurring on the number plate.
+def detect_plate(
+    img, text=""
+):  # the function detects and perfors blurring on the number plate.
     plate_img = img.copy()
     roi = img.copy()
-    plate_rect = plate_cascade.detectMultiScale(plate_img, scaleFactor=1.2,
-                                                minNeighbors=7)  # detects numberplates and returns the coordinates and dimensions of detected license plate's contours.
-    for (x, y, w, h) in plate_rect:
-        roi_ = roi[y:y + h, x:x + w, :]  # extracting the Region of Interest of license plate for blurring.
-        plate = roi[y:y + h, x:x + w, :]
-        cv2.rectangle(plate_img, (x + 2, y), (x + w - 3, y + h - 5), (51, 181, 155),
-                      3)  # finally representing the detected contours by drawing rectangles around the edges.
-    if text != '':
-        plate_img = cv2.putText(plate_img, text, (x - w // 2, y - h // 2),
-                                cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (51, 181, 155), 1, cv2.LINE_AA)
+    plate_rect = plate_cascade.detectMultiScale(
+        plate_img, scaleFactor=1.2, minNeighbors=7
+    )  # detects numberplates and returns the coordinates and dimensions of detected license plate's contours.
+    for x, y, w, h in plate_rect:
+        # roi_ = roi[
+        #     y : y + h, x : x + w, :
+        # ]  # extracting the Region of Interest of license plate for blurring.
+        plate = roi[y : y + h, x : x + w, :]
+        cv2.rectangle(
+            plate_img, (x + 2, y), (x + w - 3, y + h - 5), (51, 181, 155), 3
+        )  # finally representing the detected contours by drawing rectangles around the edges.
+    if text != "":
+        plate_img = cv2.putText(
+            plate_img,
+            text,
+            (x - w // 2, y - h // 2),
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            0.5,
+            (51, 181, 155),
+            1,
+            cv2.LINE_AA,
+        )
 
     return plate_img, plate  # returning the processed image.
 
 
-def display(img_, title=''):
+def display(img_, title=""):
     if img_ is None:  # Check if image loading was successful
         print("Error: Could not load image. Please verify the path.")
         return
 
     img = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
-    fig = plt.figure(figsize=(10, 6))
+    # fig = plt.figure(figsize=(10, 6))
     ax = plt.subplot(111)
     ax.imshow(img)
-    plt.axis('off')
+    plt.axis("off")
     plt.title(title)
     plt.show()
 
@@ -53,20 +64,26 @@ def find_contours(dimensions, img):
     cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
 
     x_cntr_list = []
-    target_contours = []
+    # target_contours = []
     img_res = []
     for cntr in cntrs:
         # detects contour in binary image and returns the coordinates of rectangle enclosing it
         intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
 
         # checking the dimensions of the contour to filter out the characters by contour's size
-        if intWidth > lower_width and intWidth < upper_width and intHeight > lower_height and intHeight < upper_height:
+        if (
+            intWidth > lower_width
+            and intWidth < upper_width
+            and intHeight > lower_height
+            and intHeight < upper_height
+        ):
             x_cntr_list.append(
-                intX)  # stores the x coordinate of the character's contour, to used later for indexing the contours
+                intX
+            )  # stores the x coordinate of the character's contour, to used later for indexing the contours
 
             char_copy = np.zeros((44, 24))
             # extracting each character using the enclosing rectangle's coordinates.
-            char = img[intY:intY + intHeight, intX:intX + intWidth]
+            char = img[intY : intY + intHeight, intX : intX + intWidth]
             char = cv2.resize(char, (20, 40))
 
             # Make result formatted for classification: invert colors
@@ -79,7 +96,9 @@ def find_contours(dimensions, img):
             char_copy[42:44, :] = 0
             char_copy[:, 22:24] = 0
 
-            img_res.append(char_copy)  # List that stores the character's binary image (unsorted)
+            img_res.append(
+                char_copy
+            )  # List that stores the character's binary image (unsorted)
 
     # Return characters on ascending order with respect to the x-coordinate (most-left character first)
 
@@ -87,7 +106,9 @@ def find_contours(dimensions, img):
     indices = sorted(range(len(x_cntr_list)), key=lambda k: x_cntr_list[k])
     img_res_copy = []
     for idx in indices:
-        img_res_copy.append(img_res[idx])  # stores character images according to their index
+        img_res_copy.append(
+            img_res[idx]
+        )  # stores character images according to their index
     img_res = np.array(img_res_copy)
 
     return img_res
@@ -97,7 +118,9 @@ def segment_characters(image):
     # Preprocess cropped license plate image
     img = cv2.resize(image, (333, 75))
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img_binary = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, img_binary = cv2.threshold(
+        img_gray, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
     img_erode = cv2.erode(img_binary, (3, 3))
     img_dilate = cv2.dilate(img_erode, (3, 3))
 
@@ -119,17 +142,17 @@ def segment_characters(image):
     return char_list
 
 
-if __name__ == '__main__':
-    img_path = './plate_detect_models/bmv.jpg'
+if __name__ == "__main__":
+    img_path = "./plate_detect_models/bmv.jpg"
     img = cv2.imread(img_path)
-    display(img, 'input image')
+    display(img, "input image")
     output_img, plate = detect_plate(img)
     char = segment_characters(plate)
-    display(output_img, 'Detected plate')
+    display(output_img, "Detected plate")
     print(len(char))
     for i in range(len(char)):
         plt.subplot(1, 10, i + 1)
-        plt.imshow(char[i], cmap='gray')
-        plt.axis('off')
+        plt.imshow(char[i], cmap="gray")
+        plt.axis("off")
 
     plt.show()
