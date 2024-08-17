e1f06fc0aa5df336b34fd572c5c7dfa78271de68
Ser-D <ipc.avanti@gmail.com>
2024-08-17 12:49:37 +0300

    Update_mypy

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,15 @@
 from pydantic_settings import BaseSettings
-from typing import Optional
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: Optional[str]
-    HOST: Optional[str] = "127.0.0.1"
-    PORT: Optional[int] = 8000
-    POSTGRES_USER: Optional[str]
-    POSTGRES_PASSWORD: Optional[str]
-    POSTGRES_DB: Optional[str]
-    POSTGRES_HOST: Optional[str]
-    POSTGRES_PORT: Optional[int]
+    DATABASE_URL: str
+    HOST: str = "127.0.0.1"
+    PORT: int = 8000
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
 
     class Config:
         env_file = ".env"
@@ -18,13 +17,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
-if (
-    settings.DATABASE_URL is None
-    or settings.POSTGRES_USER is None
-    or settings.POSTGRES_PASSWORD is None
-    or settings.POSTGRES_DB is None
-    or settings.POSTGRES_HOST is None
-    or settings.POSTGRES_PORT is None
-):
-    raise ValueError("Some required settings are missing")
