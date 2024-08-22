92c9fe834ea41fab84269a1faf7fa5b04a07803e
SergRusetskiy1974 <sergrus1974@gmail.com>
2024-08-23 01:34:44 +0300

    fix ds

--- app/routers/parking.py
+++ app/routers/parking.py
@@ -5,7 +5,7 @@ import numpy as np
 from app.services.license_plate_detector_base import detector, recognizer
 from typing import List
 
-from fastapi import APIRouter, Depends, status, Query, UploadFile, HTTPException
+from fastapi import APIRouter, Depends, status, Query, UploadFile, HTTPException, File
 from app.models.users import User
 from app.schemas.parking import ParkingCreate, ParkingResponse
 from app.services.parking import ParkingService
@@ -15,47 +15,16 @@ from app.utils.guard import guard
 router = APIRouter(prefix="/parking", tags=["Parking"])
 
 
-
-
-@router.post("/detect")
-async def plate_detector(file: UploadFile):
+@router.post("/detector")
+async def plate_detector(file: UploadFile = File(...)):
     try:
-        # print(f"plate_recognize : {file}")
-        image = await file.read()  # Read the file content as bytes
-
-        # Завантажте зображення безпосередньо з байтів
+        image = await file.read()
         img_array = np.frombuffer(image, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
-        img = detector.resize_image(img)
-        plate_img, plates = detector.detect_plate(img, "Sample Plate")
-        output_list_number = set()
-        # detector.display(plate_img, "Find Plate")
-        detector.display(plate_img, "Find Plate")
-        for plate in plates:
-
-            chars_list = detector.segment_characters(plate)
-            # if len(chars_list) > 0:
-            #     # detector.display(plate, "Find Plate")
-            #     # print(len(chars_list))
-            #     plt.figure(figsize=(3, 1))
-            #     for i in range(len(chars_list)):
-            #         plt.subplot(1, 10, i + 1)
-            #         plt.imshow(chars_list[i], cmap="gray")
-            #         plt.axis("off")
-            #
-            #     plt.show()
-            plate_text = recognizer.segment_characters(chars_list)
-            if len(plate_text) > 6:
-                output_list_number.add(plate_text)
-        for rec in output_list_number:
-            print(rec)
-            # print(f"{result=}")
-            return {"result": list(output_list_number)}
+        return detector.load_and_detect(img, "Sample Plate")
     except Exception as e:
         print({str(e)})
         raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
-
 
 
 @router.post("/", response_model=ParkingResponse, status_code=status.HTTP_201_CREATED)
@@ -65,17 +34,16 @@ async def start_parking(
         parking_service: ParkingService = Depends(),
         current_user: User = Depends(guard.is_admin),
 ):
-
     parking = await parking_service.start_parking(uow, license_plate=parking_data.license_plate)
     return parking
 
 
 @router.put("/{parking_id}/complete", response_model=ParkingResponse, status_code=status.HTTP_200_OK)
 async def complete_parking(
-    license_plate: str,
-    uow: UOWDep,
-    parking_service: ParkingService = Depends(),
-    current_user: User = Depends(guard.is_admin),
+        license_plate: str,
+        uow: UOWDep,
+        parking_service: ParkingService = Depends(),
+        current_user: User = Depends(guard.is_admin),
 ):
     parking = await parking_service.complete_parking(uow, license_plate=license_plate)
     return parking
@@ -83,11 +51,10 @@ async def complete_parking(
 
 @router.get("/", response_model=List[ParkingResponse], status_code=status.HTTP_200_OK)
 async def get_parkings(
-    uow: UOWDep,
-    active_only: bool = Query(False, description="Filter active parkings only"),
-    parking_service: ParkingService = Depends(),
-    current_user: User = Depends(guard.is_admin),
+        uow: UOWDep,
+        active_only: bool = Query(False, description="Filter active parkings only"),
+        parking_service: ParkingService = Depends(),
+        current_user: User = Depends(guard.is_admin),
 ):
     parkings = await parking_service.get_parkings(uow, active_only=active_only)
     return parkings
-
--- app/services/license_plate_detector_base.py
+++ app/services/license_plate_detector_base.py
@@ -4,58 +4,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-
-
-class Detector(ABC):
-
-    @staticmethod
-    def display(img_, title=""):
-        if img_ is None:
-            print("Error: Could not load image. Please verify the path.")
-            return
-
-        img = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
-        ax = plt.subplot(111)
-        ax.imshow(img)
-        plt.axis("off")
-        plt.title(title)
-        plt.show()
+from fastapi import HTTPException
 
 
 class LicensePlateDetector:
     def __init__(self, cascade_path="app/ds_models/indian_license_plate.xml"):
         self.plate_cascade = cv2.CascadeClassifier(cascade_path)
 
-    def detect_plate(self, img, text=""):
+    # def detect_plate(self, img, text=""):
+    #     img = self.resize_image(img)
+    #     plate_img = img.copy()
+    #     roi = img.copy()
+    #     # all_plates = []
+    #
+    #     # for scale in np.arange(1.05, 1.35, 0.05):
+    #     plate_rects = self.plate_cascade.detectMultiScale(
+    #         plate_img, scaleFactor=1.05, minNeighbors=8
+    #     )
+    #     width_max = 0  # використовується для сортування за шириною
+    #     plate_max = None
+    #     x_max = 0
+    #     y_max = 0
+    #     for x, y, w, h in plate_rects:
+    #         # коєфіцієнти збільшення розміру
+    #         a, b = (int(0.1 * h), int(0.1 * w))
+    #         aa, bb = (int(0.1 * h), int(0.1 * w))
+    #         if h > 75:  # пропускає розбиття за шириною високоякісного зображення
+    #             b = 0
+    #             bb = 0
+    #
+    #         plate = roi[y + a: y + h - aa, x + b: x + w - bb, :]
+    #
+    #         if width_max < w:
+    #             plate_max = plate
+    #             width_max = w
+    #             x_max = x
+    #             y_max = y
+    #
+    #         cv2.rectangle(plate_img, (x + 2, y), (x + w - 3, y + h - 5), (51, 224, 172), 3)
+    #
+    #     if text != '':
+    #         h = plate_max.shape[0]
+    #         plate_img = cv2.putText(plate_img, text, (x_max, y_max - h // 3),
+    #                                 cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (51, 224, 172), 2, cv2.LINE_AA)
+    #
+    #     return plate_img, plate_max
+
+    def detect_plate(self,img, text=''):
+        img = self.resize_image(img)
         plate_img = img.copy()
         roi = img.copy()
-        all_plates = []
+        plate_rect = self.plate_cascade.detectMultiScale(plate_img, scaleFactor=1.2, minNeighbors=7)
 
-        for scale in np.arange(1.05, 1.35, 0.05):
-            plate_rects = self.plate_cascade.detectMultiScale(
-                plate_img, scaleFactor=scale, minNeighbors=8
-            )
+        best_plate = None
+        max_width = 0
+        min_ratio = 2.0  # Мінімальне допустиме співвідношення ширини до висоти
+        max_ratio = 8.0  # Максимальне допустиме співвідношення ширини до висоти
 
-            for x, y, w, h in plate_rects:
-                plate = roi[y:y + h, x:x + w, :]
-                all_plates.append(plate)
-                cv2.rectangle(
-                    plate_img, (x + 2, y), (x + w - 3, y + h - 5), (51, 181, 155), 3
-                )
+        for (x, y, w, h) in plate_rect:
+            ratio = w / h
 
-        if text != "":
-            plate_img = cv2.putText(
-                plate_img,
-                text,
-                (x - w // 2, y - h // 2),
-                cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                0.5,
-                (51, 181, 155),
-                1,
-                cv2.LINE_AA,
-            )
+            # Вибираємо найширший прямокутник у межах допустимого співвідношення сторін
+            if min_ratio <= ratio <= max_ratio and w > max_width:
+                max_width = w
+                best_plate = (x, y, w, h)
 
-        return plate_img, all_plates
+            cv2.rectangle(plate_img, (x + 2, y), (x + w - 3, y + h - 5), (51, 181, 155), 3)
+
+        if best_plate:
+            x, y, w, h = best_plate
+
+            # Розширюємо прямокутник
+            expansion_factor = 0.1  # Збільшуємо ширину на 10%
+            new_w = int(w * (1 + expansion_factor))
+            new_x = max(0, x - (new_w - w) // 2)
+
+            # Переконуємося, що розширений прямокутник не виходить за межі зображення
+            new_x = min(new_x, img.shape[1] - new_w)
+
+            plate = roi[y:y + h, new_x:new_x + new_w, :]
+
+            if text != '':
+                plate_img = cv2.putText(plate_img, text, (new_x - new_w // 2, y - h // 2),
+                                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (51, 181, 155), 1, cv2.LINE_AA)
+
+            # Малюємо розширений прямокутник
+            cv2.rectangle(plate_img, (new_x + 2, y), (new_x + new_w - 3, y + h - 5), (0, 255, 0), 3)
+        else:
+            plate = None
+
+        return plate_img, plate
 
     @staticmethod
     def display(img_, title=""):
@@ -69,6 +107,13 @@ class LicensePlateDetector:
         plt.axis("off")
         plt.title(title)
         plt.show()
+
+    @staticmethod
+    def resize_image(img, target_height=640):
+        ratio = target_height / float(img.shape[0])
+        target_width = int(img.shape[1] * ratio)
+        resized = cv2.resize(img, (target_width, target_height), interpolation=cv2.INTER_AREA)
+        return resized
 
     @staticmethod
     def find_contours(dimensions, img):
@@ -80,11 +125,7 @@ class LicensePlateDetector:
         img_res = []
         for cntr in cntrs:
             intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
-            if (
-                    lower_width < intWidth < upper_width
-                    and intHeight > lower_height
-                    and intHeight < upper_height
-            ):
+            if lower_width < intWidth < upper_width and upper_height > intHeight > lower_height:
                 x_cntr_list.append(intX)
                 char_copy = np.zeros((44, 24))
                 char = img[intY: intY + intHeight, intX: intX + intWidth]
@@ -115,48 +156,79 @@ class LicensePlateDetector:
         img_dilate[72:75, :] = 255
         img_dilate[:, 330:333] = 255
 
-        dimensions = [LP_WIDTH / 6, LP_WIDTH / 2, LP_HEIGHT / 10, 2 * LP_HEIGHT / 3]
+        dimensions = [LP_WIDTH / 8, LP_WIDTH / 2, LP_HEIGHT / 10, 2 * LP_HEIGHT / 3]
         char_list = self.find_contours(dimensions, img_dilate)
         return char_list
 
-    def load_and_detect(self, image_path, text=""):
-        img = cv2.imread(image_path)
+    def load_and_detect(self, img, text=""):
+        # img = cv2.imread(image_path)
         if img is None:
-            print("Error: Could not load image. Please verify the path.")
-            return None, None
+            return HTTPException(status_code=400, detail="Could not load image. Please verify the path.")
         plate_img, plate = self.detect_plate(img, text)
+        chars_list = detector.segment_characters(plate)
+        plate_text = recognizer.segment_characters(chars_list)
         self.display(plate_img, "Detected Plate")
-        return plate_img, plate
+        return {"result": plate_text}
+
+
+class CharacterRecognizer:
+    def __init__(self, model_path="app/ds_models/plate_detect_model_new.tflite"):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
 
     @staticmethod
-    def resize_image(img, target_height=640):
-        """
-        Змінює розмір зображення до заданої висоти, зберігаючи співвідношення сторін.
-        """
-        # Розраховуємо співвідношення сторін
-        ratio = target_height / float(img.shape[0])
-        # Обчислюємо нову ширину
-        target_width = int(img.shape[1] * ratio)
-        # Змінюємо розмір зображення
-        resized = cv2.resize(img, (target_width, target_height), interpolation=cv2.INTER_AREA)
-        return resized
+    def fix_dimension(img):
+        new_img = np.zeros((28, 28, 1))
+        for i in range(1):
+            new_img[:, :, i] = img
+        return new_img
 
+    def predict_image(self, img_array):
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array.astype(np.float32) / 255.0
+        self.interpreter.set_tensor(self.input_details[0]['index'], img_array)
+        self.interpreter.invoke()
+        output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+
+        return np.argmax(output_data, axis=1)[0]
+
+    def segment_characters(self, chars):
+        dic = {}
+        characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        for i, c in enumerate(characters):
+            dic[i] = c
+
+        output = []
+        for ch in chars:  # iterating over the characters
+            img_ = cv2.resize(ch, (28, 28), interpolation=cv2.INTER_AREA)
+            img = self.fix_dimension(img_)
+            img = img.reshape(28, 28, 1)
+
+            y_ = self.predict_image(img)
+
+            character = dic[y_]  # визначаємо символ за індексом
+            output.append(character)  # зберігаємо результат у список
+
+        plate_number = ''.join(output)
+
+        return plate_number
+
+
+recognizer = CharacterRecognizer()
 
 detector = LicensePlateDetector()
 
 
 class AutoDetector:
-    def __init__(self):
-        # Завантажте TFLite модель
-        self.interpreter = tf.lite.Interpreter(model_path="./models/plate_detect_model_new.tflite")
-        self.interpreter.allocate_tensors()  # Викликайте allocate_tensors тут
-
-        # Отримайте деталі входу та виходу
+    def __init__(self, model_path="./models/plate_detect_model_new.tflite"):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
     def predict_image(self, image_path):
-        # Завантажте та підготуйте зображення
         img = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
         img_array = tf.keras.preprocessing.image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)  # Додайте вимір для пакетного розміру
@@ -179,64 +251,6 @@ class AutoDetector:
             print("На зображенні відсутній автомобіль.")
             return False
 
-
 # Використання класу AutoDetector
 # detector_auto = AutoDetector()
 # detector_auto.predict_image("./data/photo2.jpg")
-
-class CharacterRecognizer:
-    def __init__(self, model_path="app/ds_models/plate_detect_model.tflite"):
-        # Завантажте TFLite модель
-        self.interpreter = tf.lite.Interpreter(model_path=model_path)
-        self.interpreter.allocate_tensors()  # Викликайте allocate_tensors тут
-
-        # Отримайте деталі входу та виходу
-        self.input_details = self.interpreter.get_input_details()
-        self.output_details = self.interpreter.get_output_details()
-
-    def fix_dimension(self, img):
-        new_img = np.zeros((28, 28, 3))
-        for i in range(3):
-            new_img[:, :, i] = img
-        return new_img
-
-    def predict_image(self, img_array):
-        # Переконайтеся, що зображення має правильну форму
-        img_array = np.expand_dims(img_array, axis=0)  # Додайте вимір для пакетного розміру
-        img_array = img_array.astype(np.float32) / 255.0  # Нормалізуйте пікселі
-
-        # Встановіть вхідний тензор
-        self.interpreter.set_tensor(self.input_details[0]['index'], img_array)
-
-        # Виконайте передбачення
-        self.interpreter.invoke()
-
-        # Отримайте результат передбачення
-        output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-
-        # Поверніть передбачений клас
-        return np.argmax(output_data, axis=1)[0]
-
-    def segment_characters(self, chars):
-        dic = {}
-        characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        for i, c in enumerate(characters):
-            dic[i] = c
-
-        output = []
-        for ch in chars:  # iterating over the characters
-            img_ = cv2.resize(ch, (28, 28), interpolation=cv2.INTER_AREA)
-            img = self.fix_dimension(img_)
-            img = img.reshape(28, 28, 3)  # preparing image for the model
-
-            # Виклик predict_image
-            y_ = self.predict_image(img)
-
-            character = dic[y_]  # визначаємо символ за індексом
-            output.append(character)  # зберігаємо результат у список
-
-        plate_number = ''.join(output)
-
-        return plate_number
-
-recognizer = CharacterRecognizer()
