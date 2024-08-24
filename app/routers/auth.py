53967254b4bfe2ea1e2cb45a02a8488b47efc1f6
Olga Mageria <olgamageria@gmail.com>
2024-08-24 18:50:29 +0300

    .

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,6 +5,9 @@ from app.schemas.users import UserSchema, UserSchemaAdd
 from app.services.auth import AuthService
 from app.services.users import UsersService
 from app.utils.dependencies import UOWDep
+from fastapi.security import OAuth2PasswordBearer
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 router = APIRouter(
     prefix="/auth",
@@ -34,34 +37,34 @@ async def login(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-# from fastapi.security import OAuth2PasswordBearer
-
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-# async def get_current_user(token: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         email: str = payload.get("sub")
-#         if email is None:
-#             raise credentials_exception
-#     except JWTError:
-#         raise credentials_exception
-#     return email
 
 
-# @router.get("/protected", response_model=UserSchema)
-# async def protected_route(
-#     uow: UOWDep,
-#     user_service: UsersService = Depends(),
-#     current_user: str = Depends(get_current_user),
-# ):
-#     user = await user_service.get_user_by_email(uow, current_user)
-#     if user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    return email
+
+
+@router.get("/protected", response_model=UserSchema)
+async def protected_route(
+    uow: UOWDep,
+    user_service: UsersService = Depends(),
+    current_user: str = Depends(get_current_user),
+):
+    user = await user_service.get_user_by_email(uow, current_user)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
